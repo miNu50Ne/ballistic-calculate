54756cfe29bf8ac780ce577b6c8bb0a3663d42c1
miNu50Ne <minu50ne@qq.com>
2025-02-17 19:15:01 +0800

    delete useless readme

---
+++ components/log/log.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file log.hpp
+ * @author miNu50Ne (minu50ne@qq.com)
+ * @brief
+ * @version 0.1
+ * @date 2025-02-01
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+#include <ctime>
+#include <iostream>
+#include <string>
+
+namespace components {} // namespace components
