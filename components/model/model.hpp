b310281ae772e7296cd8d700b1dbde4e941b4f89
miNu50Ne <minu50ne@qq.com>
2025-02-25 19:11:05 +0800

    Add component model

---
+++ components/model/model.hpp
@@ -0,0 +1,29 @@
+#include <eigen3/Eigen/Dense>
+#include <eigen3/Eigen/src/Core/Matrix.h>
+namespace components::model {
+class Model {
+public:
+    Model() {}
+
+    void set_time_start(double time_start_set) { time_start_ = time_start_set; }
+    void set_time_end(double time_end_set) { time_end_ = time_end_set; }
+    void set_time_step(double time_step_set) { time_step_ = time_step_set; }
+
+    double time_start() { return time_start_; }
+    double time_end() { return time_end_; }
+    double time_step() { return time_step_; }
+
+    double velocity_x() { return velocity_.x(); }
+    double velocity_y() { return velocity_.y(); }
+    double velocity_z() { return velocity_.z(); }
+
+    Eigen::Vector3d velocity() { return velocity_; }
+
+private:
+    double time_start_;
+    double time_end_;
+    double time_step_;
+
+    Eigen::Vector3d velocity_;
+};
+} // namespace components::model
